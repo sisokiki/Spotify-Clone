4d18b7d22145c7fbe393b1881ce06c449f2882eb
sisokiki <bitmishra00054@gmail.com>
2026-05-19 23:07:20 +0530

    Fix routing 404 issue by switching to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import PlayerContextProvider from "./context/PlayerContext.jsx";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
